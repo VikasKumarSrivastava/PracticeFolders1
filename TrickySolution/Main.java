16df142dac981339222428fd0889a77369571b6f
“VikasSrivastava” <princevikas99@gmail.com>
2025-02-08 21:41:54 +0530

    code committed

--- TrickySolution/Main.java
+++ TrickySolution/Main.java
@@ -2,14 +2,37 @@ package TrickySolution;
 
 public class Main {
 
-
     public static void main(String[] args) {
-        System.out.println("Hello World!");
         int freq[]= new int[125];
         freq['c']++;
-        for(int i:freq){
-            System.out.println(i);
-        }
+        // for(int i:freq){
+        //     System.out.println(i);
+        // }
+        //Palindrome check
+        // String str ="hannah";
+        // System.out.println(str.substring(0,6));
+        // int i = 0;
+        // int j= str.length()-1;
+        // while(i<j){
+        //     if(str.charAt(i)!=str.charAt(j)){
+        //         System.out.println("Not a palindrome");
+        //         return;
+        //     }
+        //     i++;
+        //     j--;
+        //     }
+        // System.out.println("Palindrome");
+        //largest odd number in a string
+        String a ="6";
+        System.out.println(a + 5);
+        System.out.println((a.charAt(0)-'0') % 2);
+        System.out.println(a + 6);
+
+
+        String[] ss={"flowers","flow","flight","fly"};
         
-    }
+        System.out.println(ss[0].charAt(0));
+        }
+
 }
+
