4222c6eaf1b7340dc7b4f1108343ad65c3daecf2
“VikasSrivastava” <princevikas99@gmail.com>
2025-02-08 15:37:50 +0530

    code committed.

---
+++ TrickySolution/Main.java
@@ -0,0 +1,15 @@
+package TrickySolution;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        int freq[]= new int[125];
+        freq['c']++;
+        for(int i:freq){
+            System.out.println(i);
+        }
+        
+    }
+}
