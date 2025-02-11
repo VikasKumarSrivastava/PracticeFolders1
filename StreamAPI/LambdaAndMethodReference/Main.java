5db885b3e293b92542d63fdd2807e9a33dc9bdab
“VikasSrivastava” <princevikas99@gmail.com>
2025-02-11 11:16:57 +0530

    code committed

--- StreamAPI/LambdaAndMethodReference/Main.java
+++ StreamAPI/LambdaAndMethodReference/Main.java
@@ -8,5 +8,12 @@ public class Main {
         System.out.println("p2 calling method"); p2.run();
         System.out.println("p1 calling method"); p1.run();
         System.out.println("p2 calling method"); p2.run();
+        Runnable p3= Printer::printNoReset;
+        Runnable p4= ()->Printer.printNoReset();
+        System.out.println("p3 calling method"); p3.run();
+        System.out.println("p4 calling method"); p4.run();
+        System.out.println("p3 calling method"); p3.run();
+        System.out.println("p4 calling method"); p4.run();
+
     }
 }
